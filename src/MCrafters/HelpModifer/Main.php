965f03b037db3719018fb7b0b47071c5283db32e
Survingo <thehydra.tk@gmail.com>
2016-05-15 09:13:00 +0200

    Revert registerEvents cause Plugin crashes

--- src/MCrafters/HelpModifer/Main.php
+++ src/MCrafters/HelpModifer/Main.php
@@ -2,6 +2,15 @@
 
 namespace MCrafters\HelpModifer;
 
+/*
+ * __  __  ____            __ _                
+ *|  \/  |/ ___|_ __ __ _ / _| |_ ___ _ __ ___ 
+ *| |\/| | |   | '__/ _` | |_| __/ _ \ '__/ __|
+ *| |  | | |___| | | (_| |  _| ||  __/ |  \__ \
+ *|_|  |_|\____|_|  \__,_|_|  \__\___|_|  |___/
+ *
+*/
+
 use pocketmine\plugin\PluginBase;
 use MCrafters\HelpModifer\command\ModifedHelpCommand;
 
@@ -9,7 +18,7 @@ class Main extends PluginBase {
 	
 	public function onEnable(){
 		$this->saveDefaultConfig();
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	//	$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getServer()->getLogger()->info("§l§cHelp§6Modifer §aEnabled§c!");
 		$this->getServer()->getCommandMap()->getCommand("help")->setLabel("help_disabled");
 		$this->getServer()->getCommandMap()->getCommand("help")->unregister($this->getServer()->getCommandMap());
